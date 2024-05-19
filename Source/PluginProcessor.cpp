b1e88dcc1c6e7e3ed7a3f8a7a08cc38ec195ed94
RoarlisF/KP31 <fangzhx@shanghaitech.edu.cn>
2024-05-18 21:39:52 -0700

    Add LFO. Return state to White noise only

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -1,27 +1,56 @@
 /*
   ==============================================================================
 
+    Copyright (c) 2022 - Gordon Webb
+
+    This file is part of Rain4Unity.
+
+    Wind4Unity1 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Wind4Unity1 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Rain4Unity.  If not, see <https://www.gnu.org/licenses/>.
+
+  ==============================================================================
+*/
+/*
+  ==============================================================================
+
     This file contains the basic framework code for a JUCE plugin processor.
 
   ==============================================================================
 */
 
 #include "PluginProcessor.h"
-#include "PluginEditor.h"
+//#include "PluginEditor.h"
 
 //==============================================================================
 Rain4UnityAudioProcessor::Rain4UnityAudioProcessor()
-#ifndef JucePlugin_PreferredChannelConfigurations
-     : AudioProcessor (BusesProperties()
-                     #if ! JucePlugin_IsMidiEffect
-                      #if ! JucePlugin_IsSynth
-                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
-                      #endif
-                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
-                     #endif
-                       )
-#endif
+    : AudioProcessor(BusesProperties()
+        .withOutput("Output", juce::AudioChannelSet::stereo(), true)
+    )
 {
+    //    Global Parameters
+    addParameter(gain = new juce::AudioParameterFloat(
+        "Master Gain", "Master Gain", 0.0f, 1.0f, 0.5f));
+    //  Wind Speed Parameters
+    addParameter(dstAmplitude = new juce::AudioParameterFloat(
+        "DstAmp", "Distant Gain", 0.0001f, 1.5f, 0.75f));
+    addParameter(windSpeed = new juce::AudioParameterFloat(
+        "windSpeed", "Wind Speed", 0.01f, 40.0f, 1.0f));
+    addParameter(dstIntensity = new juce::AudioParameterFloat(
+        "intensity", "dst Intensity", 1.0f, 50.0f, 30.0f));
+    addParameter(dstResonance = new juce::AudioParameterFloat(
+        "dstResonance", "dst Resonance", 0.1f, 50.0f, 1.0f));
+    addParameter(dstPan = new juce::AudioParameterFloat(
+        "dstPan", "Distant Pan", 0.0f, 1.0f, 0.5f));
 }
 
 Rain4UnityAudioProcessor::~Rain4UnityAudioProcessor()
@@ -29,72 +58,17 @@ Rain4UnityAudioProcessor::~Rain4UnityAudioProcessor()
 }
 
 //==============================================================================
-const juce::String Rain4UnityAudioProcessor::getName() const
-{
-    return JucePlugin_Name;
-}
-
-bool Rain4UnityAudioProcessor::acceptsMidi() const
-{
-   #if JucePlugin_WantsMidiInput
-    return true;
-   #else
-    return false;
-   #endif
-}
-
-bool Rain4UnityAudioProcessor::producesMidi() const
-{
-   #if JucePlugin_ProducesMidiOutput
-    return true;
-   #else
-    return false;
-   #endif
-}
-
-bool Rain4UnityAudioProcessor::isMidiEffect() const
-{
-   #if JucePlugin_IsMidiEffect
-    return true;
-   #else
-    return false;
-   #endif
-}
-
-double Rain4UnityAudioProcessor::getTailLengthSeconds() const
-{
-    return 0.0;
-}
-
-int Rain4UnityAudioProcessor::getNumPrograms()
-{
-    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
-}
-
-int Rain4UnityAudioProcessor::getCurrentProgram()
-{
-    return 0;
-}
-
-void Rain4UnityAudioProcessor::setCurrentProgram (int index)
-{
-}
-
-const juce::String Rain4UnityAudioProcessor::getProgramName (int index)
-{
-    return {};
-}
-
-void Rain4UnityAudioProcessor::changeProgramName (int index, const juce::String& newName)
-{
-}
-
-//==============================================================================
 void Rain4UnityAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
-    // Use this method as the place to do any pre-playback
-    // initialisation that you need..
+    //    Create DSP Spec
+    juce::dsp::ProcessSpec spec;
+    spec.maximumBlockSize = samplesPerBlock;
+    spec.numChannels = 1;
+    spec.sampleRate = sampleRate;
+    currentSpec = spec;
+
+    //    Prepare DSP
+    Prepare(spec);
 }
 
 void Rain4UnityAudioProcessor::releaseResources()
@@ -103,59 +77,15 @@ void Rain4UnityAudioProcessor::releaseResources()
     // spare memory, etc.
 }
 
-#ifndef JucePlugin_PreferredChannelConfigurations
-bool Rain4UnityAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
-{
-  #if JucePlugin_IsMidiEffect
-    juce::ignoreUnused (layouts);
-    return true;
-  #else
-    // This is the place where you check if the layout is supported.
-    // In this template code we only support mono or stereo.
-    // Some plugin hosts, such as certain GarageBand versions, will only
-    // load plugins that support stereo bus layouts.
-    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
-        return false;
-
-    // This checks if the input layout matches the output layout
-   #if ! JucePlugin_IsSynth
-    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
-        return false;
-   #endif
-
-    return true;
-  #endif
-}
-#endif
-
 void Rain4UnityAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
 {
     juce::ScopedNoDenormals noDenormals;
-    auto totalNumInputChannels  = getTotalNumInputChannels();
-    auto totalNumOutputChannels = getTotalNumOutputChannels();
+    buffer.clear();
 
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
-    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        buffer.clear (i, 0, buffer.getNumSamples());
+    updateSettings();
+    dstProcess(buffer);
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer (channel);
-
-        // ..do something to the data...
-    }
+    buffer.applyGain(gain->get());
 }
 
 //==============================================================================
@@ -166,21 +96,118 @@ bool Rain4UnityAudioProcessor::hasEditor() const
 
 juce::AudioProcessorEditor* Rain4UnityAudioProcessor::createEditor()
 {
-    return new Rain4UnityAudioProcessorEditor (*this);
+    return new juce::GenericAudioProcessorEditor(*this);
 }
 
 //==============================================================================
-void Rain4UnityAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+
+void Rain4UnityAudioProcessor::Prepare(const juce::dsp::ProcessSpec& spec)
 {
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
+    //    Prepare DST
+    dstBPF.prepare(spec);
+    dstBPF.setType(juce::dsp::StateVariableTPTFilterType::bandpass);
+    dstBPF.setCutoffFrequency(10.0f);
+    dstBPF.setResonance(1.0f);
+    dstBPF.reset();
+
+    //    Prepare Whistle
+    whsBPF1.prepare(spec);
+    whsBPF1.setType(juce::dsp::StateVariableTPTFilterType::bandpass);
+    whsBPF1.setCutoffFrequency(1000.0f);
+    whsBPF1.setResonance(60.0f);
+    whsBPF1.reset();
+
+    whsBPF2.prepare(spec);
+    whsBPF2.setType(juce::dsp::StateVariableTPTFilterType::bandpass);
+    whsBPF2.setCutoffFrequency(1000.0f);
+    whsBPF2.setResonance(60.0f);
+    whsBPF2.reset();
+
+    //    Prepare Howl
+    howlBPF1.prepare(spec);
+    howlBPF1.setType(juce::dsp::StateVariableTPTFilterType::bandpass);
+    howlBPF1.setCutoffFrequency(400.0f);
+    howlBPF1.setResonance(40.0f);
+    howlBPF1.reset();
+
+    howlBPF2.prepare(spec);
+    howlBPF2.setType(juce::dsp::StateVariableTPTFilterType::bandpass);
+    howlBPF2.setCutoffFrequency(200.0f);
+    howlBPF2.setResonance(40.0f);
+    howlBPF2.reset();
+
+    howlOsc1.initialise([](float x) { return std::sin(x); }, 128);
+    howlOsc2.initialise([](float x) { return std::sin(x); }, 128);
+
+    howlBlockLPF1.prepare(0.5f, spec.maximumBlockSize, spec.sampleRate);
+    howlBlockLPF2.prepare(0.4f, spec.maximumBlockSize, spec.sampleRate);
 }
 
-void Rain4UnityAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+void Rain4UnityAudioProcessor::dstProcess(juce::AudioBuffer<float>& buffer)
 {
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
+    //    Get Buffer info
+    int numSamples = buffer.getNumSamples();
+    float FrameAmp = dstAmplitude->get();
+
+    //    Distant Wind DSP Loop
+    float pan[2];
+    cosPan(pan, dstPan->get());
+
+    for (int s = 0; s < numSamples; ++s)
+    {
+        float output = dstBPF.processSample(0, r.nextFloat() * 2.0f - 1.0f) * FrameAmp;
+        buffer.addSample(0, s, output * pan[0]);
+        buffer.addSample(1, s, output * pan[1]);
+    }
+  
+    dstBPF.snapToZero();
+}
+
+
+
+
+void Rain4UnityAudioProcessor::updateSettings()
+{
+    //  UpdateWSCircularBuffer;
+
+    float currentWindSpeed = windSpeed->get();
+    gd.windSpeedCircularBuffer[gd.wSCBWriteIndex] = currentWindSpeed;
+    ++gd.wSCBWriteIndex;
+    gd.wSCBWriteIndex = (gd.wSCBWriteIndex < wSCBSize) ? gd.wSCBWriteIndex : 0;
+
+
+    // Update DST
+    float currentDstIntensity = dstIntensity->get();
+    float currentDstResonance = dstResonance->get();
+
+    // Update DST Filter Settings
+    dstBPF.setCutoffFrequency(currentWindSpeed * currentDstIntensity);
+    dstBPF.setResonance(currentDstResonance);
+
+    /*// Update Whistle
+    
+    wd.whsWSCBReadIndex1 = gd.wSCBWriteIndex - (int)(pd.whistlePan1 * maxPanFrames);
+    wd.whsWSCBReadIndex1 = (wd.whsWSCBReadIndex1 < 0) ? wd.whsWSCBReadIndex1 + wSCBSize : wd.whsWSCBReadIndex1;
+    wd.whsWSCBReadIndex2 = gd.wSCBWriteIndex - (int)(pd.whistlePan2 * maxPanFrames);
+    wd.whsWSCBReadIndex2 = (wd.whsWSCBReadIndex2 < 0) ? wd.whsWSCBReadIndex2 + wSCBSize : wd.whsWSCBReadIndex2;
+    wd.whsWindSpeed1 = gd.windSpeedCircularBuffer[wd.whsWSCBReadIndex1];
+    wd.whsWindSpeed2 = gd.windSpeedCircularBuffer[wd.whsWSCBReadIndex2];
+    whsBPF1.setCutoffFrequency(wd.whsWindSpeed1 * 8.0f + 600.0f);
+    whsBPF2.setCutoffFrequency(wd.whsWindSpeed2 * 20.0f + 1000.0f);
+
+    //    Update Howl
+    hd.howlWSCBReadIndex1 = gd.wSCBWriteIndex - (int)(pd.howlPan1 * maxPanFrames);
+    hd.howlWSCBReadIndex1 = (hd.howlWSCBReadIndex1 < 0) ? hd.howlWSCBReadIndex1 + wSCBSize : hd.howlWSCBReadIndex1;
+    hd.howlWSCBReadIndex2 = gd.wSCBWriteIndex - (int)(pd.howlPan2 * maxPanFrames);
+    hd.howlWSCBReadIndex2 = (hd.howlWSCBReadIndex2 < 0) ? hd.howlWSCBReadIndex2 + wSCBSize : hd.howlWSCBReadIndex2;
+    hd.howlWindSpeed1 = gd.windSpeedCircularBuffer[hd.howlWSCBReadIndex1];
+    hd.howlWindSpeed2 = gd.windSpeedCircularBuffer[hd.howlWSCBReadIndex2];*/
+}
+
+void Rain4UnityAudioProcessor::cosPan(float* output, float pan)
+{
+    output[0] = juce::dsp::FastMathApproximations::cos((pan * 0.25f - 0.5f) * juce::MathConstants<float>::twoPi);
+    output[1] = juce::dsp::FastMathApproximations::cos((pan * 0.25f - 0.25f) * juce::MathConstants<float>::twoPi);
 }
 
 //==============================================================================
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -1,6 +1,28 @@
 /*
   ==============================================================================
 
+    Copyright (c) 2022 - Gordon Webb
+
+    This file is part of Rain4Unity.
+
+    Wind4Unity1 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Wind4Unity1 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Rain4Unity.  If not, see <https://www.gnu.org/licenses/>.
+
+  ==============================================================================
+*/
+/*
+  ==============================================================================
+
     This file contains the basic framework code for a JUCE plugin processor.
 
   ==============================================================================
@@ -12,11 +34,40 @@
 
 //==============================================================================
 /**
+ A Single pole low pass filter
 */
+class BlockLPF
+{
+public:
+    BlockLPF()
+    {
+    }
+    
+    ~BlockLPF()
+    {
+    }
+
+    void prepare(float cutoff, int samplesPerBlock, double sampleRate)
+    {
+        float c0 = std::tan(juce::MathConstants<double>::pi * cutoff / (sampleRate / samplesPerBlock));
+        coeff = c0 / (1 + c0);
+    }
+
+    float processSample(float input)
+    {
+        lastOutput = (1.0f - coeff) * lastOutput + coeff * input;
+        lastOutput = lastOutput < 0.00001f ? 0.0f : lastOutput;
+        return lastOutput;
+    }
+
+private:
+
+    float coeff;
+    float lastOutput;
+
+};
+
 class Rain4UnityAudioProcessor  : public juce::AudioProcessor
-                            #if JucePlugin_Enable_ARA
-                             , public juce::AudioProcessorARAExtension
-                            #endif
 {
 public:
     //==============================================================================
@@ -28,7 +79,7 @@ public:
     void releaseResources() override;
 
    #ifndef JucePlugin_PreferredChannelConfigurations
-    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override{ return true; }
    #endif
 
     void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
@@ -38,25 +89,104 @@ public:
     bool hasEditor() const override;
 
     //==============================================================================
-    const juce::String getName() const override;
+    const juce::String getName() const override{ return JucePlugin_Name; }
 
-    bool acceptsMidi() const override;
-    bool producesMidi() const override;
-    bool isMidiEffect() const override;
-    double getTailLengthSeconds() const override;
-
-    //==============================================================================
-    int getNumPrograms() override;
-    int getCurrentProgram() override;
-    void setCurrentProgram (int index) override;
-    const juce::String getProgramName (int index) override;
-    void changeProgramName (int index, const juce::String& newName) override;
+    bool acceptsMidi() const override{ return false; }
+    bool producesMidi() const override{ return false; }
+    bool isMidiEffect() const override{ return false; }
+    double getTailLengthSeconds() const override { return 0.0; }
 
     //==============================================================================
-    void getStateInformation (juce::MemoryBlock& destData) override;
-    void setStateInformation (const void* data, int sizeInBytes) override;
+    int getNumPrograms() override { return 1; }
+    int getCurrentProgram() override { return 0; }
+    void setCurrentProgram (int index) override {}
+    const juce::String getProgramName (int index) override { return {}; }
+    void changeProgramName (int index, const juce::String& newName) override {}
+
+    //==============================================================================
+    void getStateInformation (juce::MemoryBlock& destData) override{}
+    void setStateInformation (const void* data, int sizeInBytes) override{}
+
+    // Data Structs
+
+    // Constants
+    static const int wSCBSize = 500;
+    static const int numOutputChannels = 2;
+    static const int maxPanFrames = 20;
+
+    struct GlobalData
+    {
+        float windSpeedCircularBuffer[wSCBSize];
+        int wSCBWriteIndex{ 0 };
+    };
+
+    struct WhistleData
+    {
+        int whsWSCBReadIndex1 = wSCBSize - 6;
+        int whsWSCBReadIndex2 = wSCBSize - 16;
+        float whsWindSpeed1;
+        float whsWindSpeed2;
+    };
+
+    struct HowlData
+    {
+        int howlWSCBReadIndex1 = wSCBSize - 6;
+        int howlWSCBReadIndex2 = wSCBSize - 51;
+        float howlWindSpeed1;
+        float howlWindSpeed2;
+    };
+
+    struct PanData
+    {
+        float whistlePan1;
+        float whistlePan2;
+        float howlPan1;
+        float howlPan2;
+    };
 
 private:
+
+    //  Wind Methods
+    void Prepare(const juce::dsp::ProcessSpec& spec);
+    void dstProcess(juce::AudioBuffer<float>& buffer);
+    void updateSettings();
+    void cosPan(float* output, float pan);  
+
+    //  Global Parameters
+    juce::AudioParameterFloat* gain;
+
+    //  Wind Speed Parameters
+    juce::AudioParameterFloat* windSpeed;
+
+    // Distant Wind Parameters
+    juce::AudioParameterFloat* dstAmplitude;
+    juce::AudioParameterFloat* dstIntensity;
+    juce::AudioParameterFloat* dstResonance;
+    juce::AudioParameterFloat* dstPan;
+
+
+    //  Distant Wind DSP Resources
+    juce::Random r;
+    juce::dsp::StateVariableTPTFilter<float> dstBPF;
+
+    // Whistle DSP Resources
+    juce::dsp::StateVariableTPTFilter<float> whsBPF1;
+    juce::dsp::StateVariableTPTFilter<float> whsBPF2;
+
+    // Howl DSP Resources
+    juce::dsp::StateVariableTPTFilter<float> howlBPF1;
+    juce::dsp::StateVariableTPTFilter<float> howlBPF2;
+    juce::dsp::Oscillator<float> howlOsc1;
+    juce::dsp::Oscillator<float> howlOsc2;
+    BlockLPF howlBlockLPF1;
+    BlockLPF howlBlockLPF2;
+
+    //  Internal Variables
+    juce::dsp::ProcessSpec currentSpec;
+
+    GlobalData gd;
+    PanData pd;
+
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Rain4UnityAudioProcessor)
 };
